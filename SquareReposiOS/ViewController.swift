b48e2592463c1d78005a262aa724fabdf0644ea7
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2025-12-23 20:18:44 +0300

    Initial Commit

---
+++ SquareReposiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareReposiOS
+//
+//  Created by Алеся Афанасенкова on 23.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
